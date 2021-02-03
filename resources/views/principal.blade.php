47748be9f82fff9ae8f0d4688035c23b368849fb
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2021-02-03 16:45:14 -0600

    Update principal.blade.php

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -84,7 +84,7 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
 
-                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                    <button type="button" id="sidebarCollapse" class=" btn-info ">
                         <i class="fas fa-align-left"></i>
                         <span>Toggle Sidebar</span>
                     </button>
