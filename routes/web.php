c8b252cb2240ea760f264ff9820620dba25683ac
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2021-02-04 16:01:53 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,30 @@ Route::get('/moldesprincipal', function () {
     return view('moldesprincipal');
 });
 
+Route::get('/sucursal_gualiqueme', function () {
+    return view('sucursal_gualiqueme');
+});
+
+Route::get('/sucursal_elparaiso', function () {
+    return view('sucursal_elparaiso');
+});
+
+Route::get('/sucursal_sanmarcos', function () {
+    return view('sucursal_sanmarcos');
+});
+
+Route::get('/sucursal_moroceli', function () {
+    return view('sucursal_moroceli');
+});
+
+Route::get('/sucursales_total', function () {
+    return view('sucursales_total');
+});
+
+Route::get('/otras_empresas', function () {
+    return view('otras_empresas');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
