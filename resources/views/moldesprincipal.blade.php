43a1f751ff2c7e3f3d3866c6b0606c4843a3e3df
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2021-02-04 16:01:21 -0600

    Update moldesprincipal.blade.php

--- resources/views/moldesprincipal.blade.php
+++ resources/views/moldesprincipal.blade.php
@@ -2,5 +2,94 @@
 
 
 @section('content')
-hola moldes
+
+
+<div class=" col imagenprincipalpadre" style=" text-align:center;">
+
+
+
+     <div class="col" >
+
+        <div class="row" style="padding:10px;"  >
+            <div class="card" style="width: 100%;">
+                <img src="plasencianegro.png" class="card-img-top" alt="Sucursal El Paraíso"  style=" height:10rem;">
+                    <div class="card-body">
+                         <button type="button"  class="btn-info" style="width:100%;" onclick="location.href = '/sucursal_elparaiso'">Sucursal El Paraíso</button> 
+                     </div>
+            </div>
+        </div>
+        
+
+        <div class="row"  style="padding:10px;" >
+            <div class="card" style="width: 100%;">
+                <img src="plasencianegro.png" class="card-img-top" alt="Sucursal Morocelí" style=" height:10rem;">
+                    <div class="card-body">
+                         <button type="button"  class="btn-info" style="width:100%;" onclick="location.href = '/sucursal_moroceli'">Sucursal Morocelí</button> 
+                    </div>
+            </div>
+        </div>      
+
+    </div>
+
+
+
+
+
+
+
+
+
+    <div class="col" >
+
+        <div class="row" style="padding:10px;"  >
+            <div class="card" style="width: 100%;">
+                <img src="plasencianegro.png" class="card-img-top" alt="Sucursal San Marcos" style=" height:10rem;">
+                    <div class="card-body">
+                         <button type="button"  class="btn-info" style="width:100%;" onclick="location.href = '/sucursal_sanmarcos'"> Sucursal San Marcos</button> 
+                    </div>
+            </div>
+        </div>
+
+        <div class="row" style="padding:10px;"  >
+            <div class="card" style="width: 100%;">
+                <img src="plasencianegro.png" class="card-img-top" alt="Otras Empresas" style=" height:10rem;">
+                    <div class="card-body">
+                        <button type="button"  class="btn-info" style="width:100%;" onclick="location.href = '/otras_empresas'">Otras Empresas</button> 
+                    </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+    <div class="col" >
+
+    <div class="row" style="padding:10px;"  >
+        <div class="card" style="width: 100%;">
+            <img src="plasencianegro.png" class="card-img-top" alt="Sucursal Gualiqueme" style=" height:10rem;">
+                <div class="card-body">
+                    <button type="button"  class="btn-info" style="width:100%;" onclick="location.href = '/sucursal_gualiqueme'">Sucursal Gualiqueme</button> 
+                </div>
+        </div>
+    </div>
+
+    <div class="row" style="padding:10px;"  >
+        <div class="card" style="width: 100%;">
+            <img src="plasencianegro.png" class="card-img-top" alt="Total Sucursales" style=" height:10rem;">
+                <div class="card-body">
+                    <button type="button"  class="btn-info" style="width:100%;" onclick="location.href = '/sucursales_total'">Total Sucursales</button> 
+                </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+</div>
+
+
 @endsection
