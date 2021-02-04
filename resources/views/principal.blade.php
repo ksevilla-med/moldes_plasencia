f7dd8fff2234db8f5497d37548031b854ad255a0
KarinaSevilla <ksevilla231996@gmail.com>
2021-02-04 08:07:08 -0600

    Update principal.blade.php

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -86,7 +86,7 @@
 
                     <button type="button" id="sidebarCollapse" class=" btn-info ">
                         <i class="fas fa-align-left"></i>
-                        <span>Toggle Sidebar</span>
+                        <span>Menú</span>
                     </button>
                     <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
