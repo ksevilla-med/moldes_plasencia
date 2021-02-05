bbe57a353751de8b6dc230d271c235d14731ad93
KarinaSevilla <ksevilla231996@gmail.com>
2021-02-05 10:08:13 -0600

    Create MoldesController.php

---
+++ app/Http/Controllers/MoldesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Moldes;
+use Illuminate\Http\Request;
+
+class MoldesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $moldes = \DB::select('call mostrar_datos_moldes(1)');
+        return view('sucursal_elparaiso')->with('moldes', $moldes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Moldes  $moldes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Moldes $moldes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Moldes  $moldes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Moldes $moldes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Moldes  $moldes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Moldes $moldes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Moldes  $moldes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Moldes $moldes)
+    {
+        //
+    }
+}
