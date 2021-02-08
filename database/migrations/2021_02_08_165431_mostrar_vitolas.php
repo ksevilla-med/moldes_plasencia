74b083303d1c46f644c2ad45dc3fb80ad398b177
KarinaSevilla <ksevilla231996@gmail.com>
2021-02-08 11:30:27 -0600

    Create 2021_02_08_165431_mostrar_vitolas.php

---
+++ database/migrations/2021_02_08_165431_mostrar_vitolas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MostrarVitolas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       DB::unprepared('  
+       CREATE PROCEDURE `mostrar_vitolas`(IN `pa_id_planta` INT )
+        
+        BEGIN
+             SELECT vitolas.id_vitola, vitolas.vitola  FROM vitolas WHERE vitolas.id_planta = pa_id_planta;
+        END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
