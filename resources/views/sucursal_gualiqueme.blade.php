fd9575b83728576fdeac70ec3c12eaf811bf9422
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2021-02-08 08:14:20 -0600

    Update sucursal_gualiqueme.blade.php

--- resources/views/sucursal_gualiqueme.blade.php
+++ resources/views/sucursal_gualiqueme.blade.php
@@ -3,7 +3,9 @@
 
 @section('content')
 
-
+<button type="button" class=" btn-info "  data-toggle="modal" data-target="#modal_agregar_moldes">
+     <span> Agregar moldes</span>
+</button>
 
 gualiqueme
 
