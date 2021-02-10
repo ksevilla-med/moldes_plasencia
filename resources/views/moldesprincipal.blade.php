c1b06ce5ce93a483606ee205722ecd6aa398c717
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2021-02-10 15:03:07 -0600

    melvin

--- resources/views/moldesprincipal.blade.php
+++ resources/views/moldesprincipal.blade.php
@@ -98,4 +98,6 @@
 </div>
 
 
+
+
 @endsection
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -13,38 +13,18 @@
 <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
 
-<!-- Font Awesome JS -->
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-
-<script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
-  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"></script>
-  
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-<!-- CIERRA LA BARRA LATERAL -->
-<script type="text/javascript">
-$(document).ready(function () {
-$('#sidebarCollapse').on('click', function () {
-$('#sidebar').toggleClass('active');});});
-</script>
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg2" style="  
-    overflow: hidden;
+<div class="wrapper">
+
+<nav class="navbar navbar-expand-lg navbar-light bg2" style="    overflow: hidden;
     top: 0;
     position:fixed;
     width: 100%; ">
-      <div class="container-fluid">
+ <div class="container-fluid">
 
-
-
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
 
 
           <div class="row" style="width:100%;">
@@ -60,7 +40,7 @@ $('#sidebar').toggleClass('active');});});
 
 
           <div class="col-md-2">
-            <h4 style="color:#fff;text-align:center;" id="titulo" ></h4>
+            <div style="color:#fff;text-align:center;" id="titulo" ></div>
           </div>
 
           <div class="col">
@@ -85,40 +65,28 @@ $('#sidebar').toggleClass('active');});});
 </li>
 
 
-<li class="nav-item">
-<a  class="nav-link"  data-toggle="modal" data-target="#modal_cerrarsesion">
-<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
-         <path d="M7.5 1v7h1V1h-1z"/>   <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
+    <li class="nav-item">
+    <a  class="nav-link"  data-toggle="modal" data-target="#modal_cerrarsesion">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
+    <path d="M7.5 1v7h1V1h-1z"/>   <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
     </svg>
-</a>                            
-    
-</li>                       
+    </a>                            
 
-</ul>
-          </div>
-        </div>
+    </li>                       
 
-
-
-
-
-
-
-        </div>
-      </div>
-    </nav>
-
-                      
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+</nav>
 
 
 
 
 
-
-
-<div class="wrapper">
-      
-            <nav id="sidebar" style="margin-top:57px;">
+<nav id="sidebar" style="margin-top:57px;">
+                 
 
             <div  style="position: fixed;  width:250px;">
 
@@ -127,7 +95,7 @@ $('#sidebar').toggleClass('active');});});
             </div>
 
             <ul class="list-unstyled components">
-            <li><a href="/moldesprincipal" onclick="titulo1()" >Moldes</a></li>
+            <li><a  class="nav-link" id="moldes" href="/moldesprincipal" >Moldes</a></li>
             <li><a href="#">Proximamente...</a></li>
             <li><a href="#">Proximamente...</a></li>
             </ul>
@@ -141,7 +109,8 @@ $('#sidebar').toggleClass('active');});});
             </ul>
             </div>
 
-            </nav>
+       </nav>
+
 
       
       
@@ -248,9 +217,11 @@ $('#sidebar').toggleClass('active');});});
 </div>
 <!-- FIN MODAL CERRAR SESION -->
 
-<div style = "margin-top:45px;">
+<div style = "margin-top:50px;">
             @yield('content')
 </div>
+
+
 
 
     </div>
@@ -258,17 +229,28 @@ $('#sidebar').toggleClass('active');});});
 
 
 
+    <!-- Font Awesome JS -->
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 
-    <script type="text/javascript">
+<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<!-- CIERRA LA BARRA LATERAL -->
+<script type="text/javascript">
+$(document).ready(function () {
+$('#sidebarCollapse').on('click', function () {
+$('#sidebar').toggleClass('active');});});
+</script>
 
-function titulo1(){ 
 
-  var titulo="Inventario de Moldes";
-
-document.getElementById('titulo').innerHTML=titulo;
-}
-
-    </script>
+  
 
  
 
