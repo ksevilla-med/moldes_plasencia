2a8cd49805d87e48dfc7ced05aecf80f4a890262
KarinaSevilla <ksevilla231996@gmail.com>
2021-02-16 07:24:40 -0600

    Update sucursal_elparaiso.blade.php

--- resources/views/sucursal_elparaiso.blade.php
+++ resources/views/sucursal_elparaiso.blade.php
@@ -245,8 +245,8 @@
 <div class="modal fade" id="modal_editar_moldes" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" style="opacity:.9;background:#212529;width=800px;">
   <div class="modal-dialog modal-dialog-centered modal-xl"  style="opacity:.9;background:#212529;width=80%">
     <div class="modal-content">
-      <div class="modal-header" id="titulo_figura"  name= "titulo_figura">
-      
+      <div class="modal-header" id="titulo_figura"  name= "titulo_figura"  >
+    
         <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close" ></button>
       </div>
 
@@ -408,8 +408,7 @@ console.info(data[i]);
 
                   <td style="padding:0px; text-align:center;    vertical-align: inherit;" >
            
-                  <a data-toggle="modal" data-target="#modal_editar_moldes"  >
-
+                  <a data-toggle="modal" data-target="#modal_editar_moldes" onclick ="datos_modal({{ $id_molde_basico = $molde->id_molde }})" >
                     <svg xmlns="http://www.w3.org/2000/svg" width=25 height="25" fill="black" class="bi bi-pencil-square" viewBox="0 0 16 16">
                       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
