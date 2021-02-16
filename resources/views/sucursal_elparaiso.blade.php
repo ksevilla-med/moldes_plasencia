45a961e1943f0babc88be56158fb7c94d3099bcd
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2021-02-16 07:08:53 -0600

    Update sucursal_elparaiso.blade.php

--- resources/views/sucursal_elparaiso.blade.php
+++ resources/views/sucursal_elparaiso.blade.php
@@ -408,7 +408,7 @@ console.info(data[i]);
 
                   <td style="padding:0px; text-align:center;    vertical-align: inherit;" >
            
-                  <a data-toggle="modal" data-target="#modal_editar_moldes" onclick ="datos_modal({{ $id_molde_basico = $molde->id_molde }})" >
+                  <a data-toggle="modal" data-target="#modal_editar_moldes"  >
 
                     <svg xmlns="http://www.w3.org/2000/svg" width=25 height="25" fill="black" class="bi bi-pencil-square" viewBox="0 0 16 16">
                       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
